8e79a3ef548f7a8c82e899484f0d1872dfaa2993
josejeffy <josejeffy@gmail.com>
2023-01-07 14:55:33 +1100

    exit added

--- notes.bat
+++ notes.bat
@@ -4,6 +4,7 @@ echo.
 echo Please select an option:
 echo 1. Create a new note
 echo 2. Search for a note
+echo 3. Exit app
 echo.
 set /p option=Enter option number: 
 cls
@@ -11,6 +12,8 @@ if "%option%" == "1" (
   call create.bat
 ) else if "%option%" == "2" (
   call search.bat
-) else (
+) else if "%option%" == "3" (
+  
+)else (
   echo Invalid option.
 )
